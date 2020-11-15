94e98a053b7ab97b7b13459218026f93ed443852
Suh Donghwi <hwidongsuh@gmail.com>
2020-11-15 09:05:04 +0900

    feat: add oc.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import * as oc from "./oc.json";
 
 function App() {
-  return (
-    <div className="App">
-      Hello, world!
-    </div>
-  );
+  return <div className="App">Hello, world! {oc.red[0]}</div>;
 }
 
 export default App;
