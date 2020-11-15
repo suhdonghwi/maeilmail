4e8097e3cce8a94c552a14f12fd02b4dd1cba9d3
Suh Donghwi <hwidongsuh@gmail.com>
2020-11-15 12:03:24 +0900

    feat: add firebase initializing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+
+import firebase from "firebase/app";
+import "firebase/auth";
 
 import Root from "./routes";
 
@@ -11,6 +14,20 @@ const RootContainer = styled.div`
 `;
 
 function App() {
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCKIUCOk3yw8Q_ku3rWu0idgirszNDiy4M",
+      authDomain: "maeilmail-f0eee.firebaseapp.com",
+      databaseURL: "https://maeilmail-f0eee.firebaseio.com",
+      projectId: "maeilmail-f0eee",
+      storageBucket: "maeilmail-f0eee.appspot.com",
+      messagingSenderId: "238455436713",
+      appId: "1:238455436713:web:a8b853744697a5ddc540b8",
+      measurementId: "G-XYJBDBP058",
+    };
+    firebase.initializeApp(firebaseConfig);
+  }, []);
+
   return (
     <RootContainer>
       <Root />
