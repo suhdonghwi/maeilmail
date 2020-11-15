9b39838d8458b555129e56c6982543f4d6bbe165
Suh Donghwi <hwidongsuh@gmail.com>
2020-11-15 13:39:51 +0900

    feat: add login page styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,44 @@
 import React from "react";
+import styled from "styled-components/macro";
+
+import oc from "../oc.json";
 
 import firebase from "firebase";
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+const Container = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${oc.indigo[5]};
+`;
+
+const LogoImage = styled.img`
+  width: 11rem;
+  margin-bottom: 2rem;
+`;
+
+const LogoText = styled.p`
+  color: white;
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
 
 export default function Login() {
   const uiConfig = {
     signInFlow: "popup",
     signInSuccessUrl: "/",
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    ],
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   };
 
   return (
-    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    <Container>
+      <LogoText>반복되는 일상을 도전의 반복으로,</LogoText>
+      <LogoImage src="/logo.png" />
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </Container>
   );
 }
